3549e8dd1d496647ac638a3b6b7600d32e7a882b
Tristan Ferré <tristanferre@yahoo.fr>
2015-08-12 17:06:24 +0200

    fix

--- launch.py
+++ launch.py
@@ -5,7 +5,7 @@ try:
   matrix_rows = 32
 
   matrix_displayer = MatrixDisplay(matrix_rows, chained_matrices)
-  matrix_displayer.start_display
-  
+  matrix_displayer.start_display()
+
 except KeyboardInterrupt:
   matrix_displayer.reset_screen
